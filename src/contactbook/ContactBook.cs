8d57251a5c4a357b8bd71de027e86454e0ed46bf
Kristian Rivera <kriss9607@gmail.com>
2026-05-04 19:31:04 -0400

    Add Delete Contact

--- src/contactbook/ContactBook.cs
+++ src/contactbook/ContactBook.cs
@@ -264,6 +264,8 @@ public class ContactBook
         Console.Write("Enter email: ");
         string email = Console.ReadLine()!;
 
+        Console.WriteLine();
+
         if (Confirm("Do you want to create this contact?", YES))
         {
             Contact c = new Contact(fname, lname, phone, email);
@@ -333,7 +335,7 @@ public class ContactBook
         string email = c.GetEmail();
 
         ReviewContact(index);
-        
+
         Console.WriteLine();
 
         if (Confirm("Do you want to edit the first name?", NO))
@@ -360,6 +362,8 @@ public class ContactBook
             email = Console.ReadLine()!;
         }
 
+        Console.WriteLine();
+
         if (Confirm("Do you want to update this contact?", YES))
         {
             c.SetFname(fname);
@@ -377,7 +381,38 @@ public class ContactBook
 
     private void DeleteContact()
     {
+        int index = GetInt("Enter index", 1, allContacts.Count) - 1;
+
+        Console.Clear();
+
+        Console.WriteLine(new string('#', 80));
         Console.WriteLine("Delete Contact");
+        Console.WriteLine(new string('#', 80));
+        Console.WriteLine();
+
+        DeleteContact(index);
+
+        Console.WriteLine();
+        PressEnterContinue();
+    }
+
+    private void DeleteContact(int index)
+    {
+        Contact c = allContacts[index];
+
+        ReviewContact(index);
+
+        Console.WriteLine();
+
+        if (Confirm("Do you want to delete this contact?", NO))
+        {
+            allContacts.Remove(c);
+            Console.WriteLine("Operation successful: Contact deleted.");
+        }
+        else
+        {
+            Console.WriteLine("Operation cancelled: Contact not deleted.");
+        }
     }
 
     private void FindContacts()
